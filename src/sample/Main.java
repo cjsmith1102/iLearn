cfb3523650ef90a53b2e81dd67b2aada3eaefd66
cjsmith1102 <cjsmith1102@gmail.com>
2016-04-06 14:50:59 -0400

    Added DB SQL Connect as well as GUI for Create Class

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -5,16 +5,22 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
-    }
+        primaryStage.setTitle("iLearn");
+        Scene rootScene = new Scene(root, 600, 500);
+        primaryStage.setScene(rootScene);
+        primaryStage.initStyle(StageStyle.UTILITY);
+      //  primaryStage.setResizable(false);
+
+
+    primaryStage.show();
+}
 
 
     public static void main(String[] args) {
